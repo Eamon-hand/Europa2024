2f8114d3e2d58de78f5d9381532d72f5ea5ab811
Eamon Hand <132242272+Eamon-hand@users.noreply.github.com>
2024-07-02 10:42:00 +1200

    camera and sprint controls

---
+++ Europa2024/Assets/scripts/playerControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float miscinputx;
+    private float miscinputy;
+    [SerializeField] private float moveSpeed = 4f;
+    public float sprint = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this code moves the player on the x and y axis
+        rb.velocity = new Vector2(miscinputx * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(rb.velocity.x, miscinputy * moveSpeed);
+        
+        // this code allows for sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            float sprintSpeed = moveSpeed + sprint;
+            moveSpeed = sprintSpeed;
+        }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 4f;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        //this defines what the fields of "miscinputx" and "miscinputy" are
+        miscinputx = context.ReadValue<Vector2>().x;
+        miscinputy = context.ReadValue<Vector2>().y;
+    }
+}
