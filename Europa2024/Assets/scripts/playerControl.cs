bfd6b4e3db167fba818618091179a20e64a965bb
Eamon Hand <132242272+Eamon-hand@users.noreply.github.com>
2024-07-23 10:31:21 +1200

    changed a LOT, added death, items, proto-enemies, and added a death zone all around the area to keep players in-bounds.

--- Europa2024/Assets/scripts/playerControl.cs
+++ Europa2024/Assets/scripts/playerControl.cs
@@ -1,14 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class playerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float miscinputx;
     private float miscinputy;
-    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float moveSpeed = 6f;
     public float sprint = 1f;
 
     // this code runs once, when the game starts
@@ -25,6 +28,7 @@ public class playerControl : MonoBehaviour
         rb.velocity = new Vector2(rb.velocity.x, miscinputy * moveSpeed);
         
         // this code allows for sprinting
+        // it isn't a very good sprint but its the thought that counts
         if (Input.GetKey(KeyCode.LeftShift))
         {
             float sprintSpeed = moveSpeed + sprint;
@@ -32,8 +36,12 @@ public class playerControl : MonoBehaviour
         }
         if (!Input.GetKey(KeyCode.LeftShift))
         {
-            moveSpeed = 4f;
+            moveSpeed = 6f;
         }
+
+        //important note: this space will be used for killing the player
+        
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -41,5 +49,19 @@ public class playerControl : MonoBehaviour
         //this defines what the fields of "miscinputx" and "miscinputy" are
         miscinputx = context.ReadValue<Vector2>().x;
         miscinputy = context.ReadValue<Vector2>().y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Lethal"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            Destroy(collision.gameObject);
+            //moveSpeed = 7f;
+        }
     }
 }
