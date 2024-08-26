5446e4aa26b9e9268bf4dcd85749694b3e8951be
Eamon Hand <132242272+Eamon-hand@users.noreply.github.com>
2024-08-27 10:30:27 +1200

    began work on a gun

--- Europa2024/Assets/scripts/TargetingSystem.cs
+++ Europa2024/Assets/scripts/TargetingSystem.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TargetingSystem : MonoBehaviour
 {
+    [SerializeField] private GameObject attackSystem;
+    
+    private Vector2 worldPosition;
+    private Vector2 direction;
+    private float angle;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,30 @@ public class TargetingSystem : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        BulletRotation();
+    }
+
+    //this code handles the aiming system rotating towards the cursor
+    private void BulletRotation()
+    {
+        //this rotates the targeting system asset
+        worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        direction = (worldPosition - (Vector2)attackSystem.transform.position).normalized;
+        attackSystem.transform.right = direction;
+
+        //this vertically flips the targeting system asset when it reaches 90 or -90 degrees
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         
+        Vector3 localScale = Vector3.one;
+        if (angle >90 | angle < -90)
+        {
+            localScale.y = -1f;
+        }
+        else
+        {
+            localScale.y = 1f;
+        }
     }
 }
