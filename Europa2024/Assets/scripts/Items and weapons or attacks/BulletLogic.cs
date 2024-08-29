7723402db5dcf039037b10556858e40b3a0c52fa
Eamon Hand <132242272+Eamon-hand@users.noreply.github.com>
2024-08-29 14:10:15 +1200

    updated the gun function; now it makes bullets

---
+++ Europa2024/Assets/scripts/Items and weapons or attacks/BulletLogic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class BulletLogic : MonoBehaviour
+//{
+    //[SerializeField] private float 
+    
+    //private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        //rb = GetComponent<Rigidbody2D>();
+
+        //SetVelocity();
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+    //whoops I ran out of time
+    //private void SetVelocity()
+    //{
+        //rb.velocity = transform.right * 
+    //}
+
+//}
--- Europa2024/Assets/scripts/Items and weapons or attacks/TargetingSystem.cs
+++ Europa2024/Assets/scripts/Items and weapons or attacks/TargetingSystem.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TargetingSystem : MonoBehaviour
 {
     [SerializeField] private GameObject attackSystem;
-    
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletLaunch;
+
+    private GameObject BulletInst;
+
     private Vector2 worldPosition;
     private Vector2 direction;
     private float angle;
@@ -21,6 +26,7 @@ public class TargetingSystem : MonoBehaviour
     private void Update()
     {
         BulletRotation();
+        BulletShooting();
     }
 
     //this code handles the aiming system rotating towards the cursor
@@ -35,13 +41,24 @@ public class TargetingSystem : MonoBehaviour
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         
         Vector3 localScale = Vector3.one;
-        if (angle >90 | angle < -90)
+        if (angle > 90 || angle < -90)
         {
             localScale.y = -1f;
         }
         else
         {
             localScale.y = 1f;
+        }
+
+        attackSystem.transform.localScale = localScale;
+    }
+
+    //this code allows us to actually "fire" bullets (create a bullet object)
+    private void BulletShooting()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            BulletInst = Instantiate(bullet, bulletLaunch.position, attackSystem.transform.rotation);
         }
     }
 }
